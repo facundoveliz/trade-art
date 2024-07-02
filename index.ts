d28dc584a53defb6875c739cd16b5f0659784b7a
Facundo Veliz <facundoveliz9@gmail.com>
2024-07-02 01:14:55 -0300

    database setup

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import express from 'express';
+import sequelize from './config/sequelize';
+
+const app = express();
+
+const port = process.env.PORT ?? 8080;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+});
+
+sequelize
+  .sync()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+    console.log('All models were synchronized successfully.');
+  })
+  .catch((err: any) => {
+    console.error('Unable to connect to the database:', err);
+  });
