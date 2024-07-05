4fa6bae73ebbbfc71eeac4a2d19bb02303fe5cf1
SantiTose <tosettisantino@gmail.com>
2024-07-05 01:26:33 -0300

    linting

--- config/sequelize.ts
+++ config/sequelize.ts
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'trade-art.sqlite'
+	dialect: "sqlite",
+	storage: "trade-art.sqlite",
 });
 
 export default sequelize;
--- index.ts
+++ index.ts
@@ -1,19 +1,20 @@
-import express from 'express';
-import sequelize from './config/sequelize';
+import express from "express";
+import sequelize from "./config/sequelize";
 
 const app = express();
 
 const port = process.env.PORT ?? 8080;
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+	console.log(`Listening on port ${port}`);
 });
 
 sequelize
-  .sync()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-    console.log('All models were synchronized successfully.');
-  })
-  .catch((err: any) => {
-    console.error('Unable to connect to the database:', err);
-  });
+	.sync()
+	.then(() => {
+		console.log("Connection has been established successfully.");
+		console.log("All models were synchronized successfully.");
+	})
+	// biome-ignore lint/suspicious/noExplicitAny: <Typescript correction>
+	.catch((err: any) => {
+		console.error("Unable to connect to the database:", err);
+	});
